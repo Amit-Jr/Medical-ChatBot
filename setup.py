998e7a451ec88d033ff14e390ceb398f97869b80
Amit Jangir <amit.jangir2902@gmail.com>
2025-07-30 22:11:38 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Medical-ChatBot",
+    version = "0.1.0",
+    author = "Amit Jangir",
+    author_email="amit.jangir2902@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
